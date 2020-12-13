81cd749436cbb1329a42e208c58717ca6c6fae65
José Fernando <jose.gomezm12@gmail.com>
2020-12-13 16:58:22 -0600

    events when starts and wwhen item end

--- src/components/YTPlayer.js
+++ src/components/YTPlayer.js
@@ -8,33 +8,64 @@ const YTPlayerStyle = {
 }
 
 class YTPlayer extends Component {
-  static contextType = CurrentVideoContext;
+  static contextType =  CurrentVideoContext ;
   constructor(props) {
     super(props);
   }
 
   componentDidMount() {
-    const [currentVideo, setCurrentVideo] = this.context
+    const [currentVideo, setCurrentVideo] = this.context;
+    const [queue, setQueue] = this.props.queueContext;
+    let videoId = null;
+    if(queue.queue) {
+      setQueue({...queue, currentIndex: 0});
+      setCurrentVideo({...queue.queue[0]});
+      videoId = queue.queue[0].videoId;
+    }
     this.player = new window['YT'].Player('YTPlayer', {
-      videoId: currentVideo.videoId,
+      videoId,
       events: {
         'onStateChange': this.onPlayerStateChange.bind(this),
         'onReady': (e) => {
-          e.target.playVideo();
-          setCurrentVideo({...currentVideo, playing: true});
+          e.target.setVolume(0);
         }
       }
     });
   }
 
   componentDidUpdate() {
-    const [currentVideo] = this.context;
+    const [currentVideo, setCurrentVideo] = this.context;
 
-    if(currentVideo.playing) {
-      this.player.playVideo();
-    } else {
-      this.player.pauseVideo();
+    if(currentVideo.hasOwnProperty('videoId')) {
+      if(currentVideo.hasOwnProperty('playing')) {
+        if(currentVideo.playing) {
+          this.player.playVideo()
+        } else {
+          this.player.pauseVideo();
+        }
+      }
     }
+    /*try {
+      if(currentVideo.hasOwnProperty('videoId')) {
+        console.log("videoId", currentVideo.hasOwnProperty('videoId'))
+        if(currentVideo.hasOwnProperty('playing')) {
+          console.log("playing", currentVideo.hasOwnProperty('playing'))
+          
+          if(currentVideo.playing) {
+            this.player.playVideo();
+          } else {
+            this.player.pauseVideo();
+          }
+        } else {
+          console.log("playing", currentVideo.hasOwnProperty('playing'), this.player)
+          this.player.loadVideoById({videoId: currentVideo.videoId})
+          setCurrentVideo({...currentVideo, playing: true})
+        }
+      }
+    } catch(e) {
+
+    }*/
+
   }
 
   render() {
@@ -44,7 +75,9 @@ class YTPlayer extends Component {
   }
 
   onPlayerStateChange(event) {
+    console.log(event);
     const[currentVideo, setCurrentVideo] = this.context;
+    const[queue, setQueue] = this.props.queueContext;
     switch (event.data) {
       case window['YT'].PlayerState.PLAYING:
         setCurrentVideo({...currentVideo, playing: true})
@@ -54,7 +87,17 @@ class YTPlayer extends Component {
         break;
       case window['YT'].PlayerState.ENDED:
         console.log('ended ');
-        setCurrentVideo({...currentVideo, playing: false})
+        
+        
+        if(queue.currentIndex < queue.queue.length - 1) {
+          //setCurrentVideo({...queue.queue[queue.currentIndex + 1]})
+          console.log("falta", )
+          this.player.loadVideoById({ videoId: queue.queue[queue.currentIndex + 1].videoId})
+          setQueue({...queue, currentIndex: queue.currentIndex + 1})
+          setCurrentVideo({...queue.queue[queue.currentIndex + 1]})
+        }
+        break;
+      case window['YT'].PlayerState.CUED:
         break;
     };
   };
