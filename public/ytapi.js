7cbd8ab555daeba95e9b2740f48a967e8725f165
José <jose.gomezm12@gmail.com>
2021-01-19 14:46:39 -0600

    iframe programatically

---
+++ public/ytapi.js
@@ -0,0 +1,4 @@
+const tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+const firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
